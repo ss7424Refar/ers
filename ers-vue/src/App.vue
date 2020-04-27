af630cdebd5bf2aab5c7e58c0fc77767a867a5a2
ss7424Refar <812647742@qq.com>
2020-04-27 16:27:21 +0800

    [Init] ers project

---
+++ ers-vue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <b-container fluid>
+      <router-view/>
+      <HelloWorld msg="Welcome to Bootstrap Table App"/>
+    </b-container>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from '@/components/HelloWorld.vue'
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    }
+  }
+</script>
